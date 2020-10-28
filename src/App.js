0bffcdd57b6b988a902f19c46ebe5a2f9bd1aad5
Julie Nginn <nginn.julie@gmail.com>
2020-10-28 10:31:31 +0100

    Feat: results page + protect results route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import {
   Route,
   HashRouter as Router,
-  Switch
+  Switch,
+  Redirect
 } from 'react-router-dom'
 import { getPath } from './helpers/routes'
 import Home from './pages/Home'
 import Quiz from './pages/Quiz'
 import Results from './pages/Results'
+import isEmpty from 'lodash/fp/isEmpty'
+
+const PrivateRoute = ({ component: Component, currentUser, ...rest }) =>
+  <Route 
+    {...rest}
+    render={props => !isEmpty(currentUser)
+      ? <Component {...props} />
+      : <Redirect to={{ pathname: getPath('home'), state: { from: props.location } }} />
+    }
+  />
 
 function App() {
+  const currentUser = useSelector(state => state.currentUser)
   return (
     <Router>
       <Switch>
         <Route exact path={getPath('home')} component={Home}></Route>
         <Route path={getPath('quiz')} component={Quiz}></Route>
-        <Route path={getPath('results')} component={Results}></Route>
+        <PrivateRoute path={getPath('results')} currentUser={currentUser} component={Results}></PrivateRoute>
       </Switch>
     </Router>
   )
--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,8 +1,65 @@
 import React from 'react'
+import styled from 'styled-components'
 import { useSelector } from 'react-redux'
 import Header from '../components/Header'
 import get from 'lodash/fp/get'
+import getOr from 'lodash/fp/getOr'
 import size from 'lodash/fp/size'
+import { BREAKPOINTS } from '../helpers/theme'
+
+const StyledCertificate = styled.div`
+  width: 60%;
+  margin: auto;
+  position: relative;
+
+  @media (max-width: ${BREAKPOINTS.sm}) {
+    width: 90%;
+  }
+`
+
+const StyledCertificateImg = styled.img`
+  width: 100%;
+`
+
+const StyledContent = styled.div`
+  position: absolute;
+  top: 35%;
+  left: 20%;
+`
+
+const StyledTitle = styled.h1`
+  @media (max-width: ${BREAKPOINTS.xs}) {
+    font-size: 20px;
+  }
+`
+
+const getGrading = score => {
+  switch (score) {
+    case 10:
+      return 'Excellent'
+    case 9:
+      return 'Très bien'
+    case 8:
+      return 'Bien'
+    case 7:
+      return 'Très suffisant'
+    case 6:
+      return 'Suffisant'
+    case 5:	
+      return 'Moyen'
+    case 4:
+      return 'Insuffisant'
+    case 3:
+      return 'Très insuffisant'
+    case 2:
+      return 'Mauvais'
+    case 1:
+    case 0:
+      return 'Très mauvais'
+    default:
+      return null
+  }
+}
 
 const Results = () => {
   const currentUser = useSelector(state => state.currentUser)
@@ -10,7 +67,13 @@ const Results = () => {
   return (
     <>
       <Header />
-      <h1>Ton score: {get('score', currentUser)} / {size(get('questions', quiz))}</h1>
+      <StyledCertificate>
+        <StyledCertificateImg src={require('../assets/certificate.png')} alt="Certificat" />
+        <StyledContent>
+          <StyledTitle>Ton score: {getOr(0, 'score', currentUser)} / {size(get('questions', quiz))}</StyledTitle>
+          <StyledTitle>{getGrading(getOr(0, 'score', currentUser))}</StyledTitle>
+        </StyledContent>
+      </StyledCertificate>
     </>
   )
 }
