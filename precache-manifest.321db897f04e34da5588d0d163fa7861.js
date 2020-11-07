9a617bc46d04d6203e77d59df9d6048a277300ce
Julie Nginn <nginn.julie@gmail.com>
2020-11-07 16:10:29 +0100

    Updates

---
+++ precache-manifest.321db897f04e34da5588d0d163fa7861.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee298a6a581b99299536c445adbacc2a",
+    "url": "/culture-quiz/index.html"
+  },
+  {
+    "revision": "a05565e6ee9062bcaaf3",
+    "url": "/culture-quiz/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "314713901ad030519bc2",
+    "url": "/culture-quiz/static/js/2.aa74e5fd.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/culture-quiz/static/js/2.aa74e5fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a05565e6ee9062bcaaf3",
+    "url": "/culture-quiz/static/js/main.f9c0313e.chunk.js"
+  },
+  {
+    "revision": "042ee381c80c2c23d8bb",
+    "url": "/culture-quiz/static/js/runtime-main.b7372fcf.js"
+  },
+  {
+    "revision": "30085cf1af87ff0ece937aafcea2a750",
+    "url": "/culture-quiz/static/media/certificate.30085cf1.png"
+  }
+]);
